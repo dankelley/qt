e48da468413025807e7605fa65af3bd922016cd9
Dan Kelley <kelley.dan@gmail.com>
2016-07-11 11:40:05 -0300

    improve plot01

--- plotting/plot01.R
+++ plotting/plot01.R
@@ -11,12 +11,16 @@ if (!interactive()) png("~/Sites/qt/plot01.png", width=7, height=3, unit="in", r
 nf <- layout(matrix(c(1,3,2,3), 2, 2, byrow = TRUE),widths=c(0.6,0.4),heights=c(0.5,0.5))
 # layout.show(nf)
 days <- substr(range(time),1,10)
-oce.plot.ts(time, t, ylab="T [C]", drawTimeRange=FALSE, mar=c(2, 3, 1, 1)) #, type='o')
+oce.plot.ts(time, t, ylab="T [C]", drawTimeRange=FALSE, mar=c(2, 3, 1, 1), col='darkred', lwd=2)
+points(time[1], t[1], col='darkred')
 if (days[1] == days[2])
     mtext(format(time[1], "%b %e, %Y"), side=3, adj=1, line=0, cex=0.7)
-oce.plot.ts(time, q, ylab="q [rel]", drawTimeRange=FALSE, mar=c(2, 3, 1, 1))
+oce.plot.ts(time, q, ylab="q [rel]", drawTimeRange=FALSE, mar=c(2, 3, 1, 1), col='darkgreen', lwd=2)
+points(time[1], q[1], col='darkgreen')
+
 par(mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 
 plot(t,q, type="l", xlab="T [C]", ylab="q [rel]")
+
 if (!interactive()) dev.off()
 

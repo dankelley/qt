a1ef2957a8d74a71c2fbe2ccadc250b141b1a8c7
Dan Kelley <kelley.dan@gmail.com>
2016-07-13 15:50:18 -0300

    new humidity variable

--- plotting/plot01.R
+++ plotting/plot01.R
@@ -1,5 +1,11 @@
 library(oce)
 library(RSQLite)
+equilibriumVaporPressure <- function(t, p=1000) {
+    ## Buck formula
+    ## https://en.wikipedia.org/wiki/Relative_humidity#Measurement
+    (1.0007 + 3.46e-6*p) * 6.1121*exp(17.502*t/(240.97+t))
+}
+
 m <- dbDriver("SQLite")
 con <- dbConnect(m, dbname="/Users/kelley/qt/database/qt.db")
 observations <- dbGetQuery(con, "select time,t,q from observations")
@@ -13,7 +19,7 @@ midnightLast <- ISOdatetime(1900+timeLast$year, timeLast$mon+1, timeLast$mday, 0
 recent <- time >= midnightLast
 
 if (!interactive()) png("~/Sites/qt/plot01.png", width=7, height=3, unit="in", res=100)
-nf <- layout(matrix(c(1,3,2,3), 2, 2, byrow = TRUE),widths=c(0.6,0.4),heights=c(0.5,0.5))
+nf <- layout(matrix(c(1,4,2,4,3,4), 3, 2, byrow = TRUE),widths=c(0.6,0.4),heights=c(0.3,0.3,0.3))
 # layout.show(nf)
 days <- substr(range(time),1,10)
 oce.plot.ts(time, t, ylab="T [C]", drawTimeRange=FALSE, mar=c(2, 3, 1, 1), col='darkred', lwd=1.2)
@@ -25,6 +31,11 @@ if (days[1] == days[2])
 oce.plot.ts(time, q, ylab="Rel. Hum. [%]", drawTimeRange=FALSE, mar=c(2, 3, 1, 1), col='darkgreen', lwd=1.2)
 lines(time[recent], q[recent], col='darkgreen', lwd=4)
 points(time[n], q[n], col='darkgreen')
+
+Q <- q / equilibriumVaporPressure(t)
+oce.plot.ts(time, Q, ylab="Equil. Vap. Pres. [mbar]", drawTimeRange=FALSE, mar=c(2, 3, 1, 1), col='darkgreen', lwd=1.2)
+lines(time[recent], Q[recent], col='darkgreen', lwd=4)
+points(time[n], Q[n], col='darkgreen')
 
 par(mar=c(3, 3, 1, 1), mgp=c(2, 0.7, 0))
 
